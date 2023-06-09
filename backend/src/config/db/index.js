5f1ebb0dd39f5eba44ebe74181b03ae2ccd0252f
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2023-06-09 23:21:05 +0700

    next config mongourl

--- backend/src/config/db/index.js
+++ backend/src/config/db/index.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://be-quizapp:SbQvqUdaZjSuHCCM@be-quizapp.icpu7lx.mongodb.net/?authSource=be-quizapp&authMechanism=SCRAM-SHA-1"
-    );
+    await mongoose.connect("mongodb://localhost:27017/quizapp");
     console.log("Connect Successfully!!");
   } catch (error) {
     console.log("Connect Fail!!");
