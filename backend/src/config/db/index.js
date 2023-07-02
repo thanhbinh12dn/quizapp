9d7cc610e6d0925ef77399f0763ba7fd451f8cce
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2023-07-02 12:36:43 +0700

    config mongodb alats

--- backend/src/config/db/index.js
+++ backend/src/config/db/index.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
+const MONGODB_URL = process.env.MONGODB_URL
+
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/quizapp");
+    // await mongoose.connect("mongodb+srv://quizapp_admin:WviT5ABBtqBYALuM@quizapp.kojtchm.mongodb.net/quizapp?retryWrites=true&w=majority");
+    await mongoose.connect(MONGODB_URL);
     console.log("Connect Successfully!!");
   } catch (error) {
     console.log("Connect Fail!!");
