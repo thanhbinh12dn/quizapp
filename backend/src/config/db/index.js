e2904fdfc62c1de7d281c08493ca8af259488064
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2023-07-02 12:41:40 +0700

    config mongodb connect

--- backend/src/config/db/index.js
+++ backend/src/config/db/index.js
@@ -5,7 +5,6 @@ const MONGODB_URL = process.env.MONGODB_URL
 
 async function connect() {
   try {
-    // await mongoose.connect("mongodb+srv://quizapp_admin:WviT5ABBtqBYALuM@quizapp.kojtchm.mongodb.net/quizapp?retryWrites=true&w=majority");
     await mongoose.connect(MONGODB_URL);
     console.log("Connect Successfully!!");
   } catch (error) {
