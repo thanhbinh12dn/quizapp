3562fe8da2cb03b32b0f407388451d54c44bbabd
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2023-07-04 13:05:16 +0700

    changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-require('dotenv').config()
+// require('dotenv').config()
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 const route = require("./routes");
 
